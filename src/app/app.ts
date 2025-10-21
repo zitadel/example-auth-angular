36a00db07ee8a5009705c0285113192804b6e1fc
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-10-21 20:53:43 +1100

    added support for env vars

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,4 +8,10 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('angular-oidc-context');
+  protected readonly envVars = {
+    domain: (import.meta as any).env['NG_APP_ZITADEL_DOMAIN'],
+    clientId: (import.meta as any).env['NG_APP_ZITADEL_CLIENT_ID'],
+    callbackUrl: (import.meta as any).env['NG_APP_ZITADEL_CALLBACK_URL'],
+    postLogoutUrl: (import.meta as any).env['NG_APP_ZITADEL_POST_LOGOUT_URL']
+  };
 }
---
+++ src/env.d.ts
@@ -0,0 +1,1 @@
+/// <reference types="@ngx-env/builder" />
