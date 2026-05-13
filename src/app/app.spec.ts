b18518452c24c83a8f43d3d1fdfc2d996d98a550
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-05-13 10:03:41 +0300

    refactor: rename Angular project to example-angular-auth and flatten dist output

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -19,7 +19,7 @@ describe('App', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, angular-oidc-context',
+      'Environment Variables Test',
     );
   });
 });
